f68858a10ac84abb94d9b5a5d8ea1f61d1a353a5
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2026-01-14 15:53:53 -0800

    [Build] Fix old file copy commands. Add custom flatpak apps.

--- build_files/03-extras.sh
+++ build_files/03-extras.sh
@@ -13,23 +13,7 @@ set -eoux pipefail
 # shellcheck source=/dev/null
 source /ctx/build/copr-helpers.sh
 
-echo "::group:: Copy Custom Files"
-
-# Copy Brewfiles to standard location
-mkdir -p /usr/share/ublue-os/homebrew/
-cp /ctx/custom/brew/*.Brewfile /usr/share/ublue-os/homebrew/
-
-# Consolidate Just Files
-mkdir -p /usr/share/ublue-os/just/
-find /ctx/custom/ujust -iname '*.just' -exec printf "\n\n" \; -exec cat {} \; >> /usr/share/ublue-os/just/60-custom.just
-
-# Copy Flatpak preinstall files
-mkdir -p /etc/flatpak/preinstall.d/
-cp /ctx/custom/flatpaks/*.preinstall /etc/flatpak/preinstall.d/
-
-echo "::endgroup::"
-
-echo "::group:: Install Packages"
+echo "::group:: Install Extra System Packages"
 
 dnf5 install -y alacritty \
     ansible \
