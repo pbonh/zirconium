953f65e129d417bf89c4461b86804861e5d65e31
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2026-01-28 11:01:26 -0800

    [Bash] Refine ble.sh config.

--- build_files/03-extras.sh
+++ build_files/03-extras.sh
@@ -46,6 +46,7 @@ copr_install_isolated "jdxcode/mise" mise
 BLESH_DIR="/var/tmp/blesh"
 git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git "${BLESH_DIR}"
 make -C "${BLESH_DIR}" install DESTDIR=/ PREFIX=/usr
+echo 'ble-attach' | tee -a "/etc/bashrc"
 rm -rf "${BLESH_DIR}"
 
 echo "::endgroup::"
@@ -63,6 +64,7 @@ install -Dpm0755 /ctx/files/etc/profile.d/fzf.sh /etc/profile.d/fzf.sh
 install -Dpm0755 /ctx/files/etc/profile.d/starship.sh /etc/profile.d/starship.sh
 install -Dpm0755 /ctx/files/etc/profile.d/zoxide.sh /etc/profile.d/zoxide.sh
 install -Dpm0755 /ctx/files/etc/profile.d/blesh.sh /etc/profile.d/blesh.sh
+install -Dpm0755 /ctx/files/etc/blerc /etc/blerc
 
 # Install and globally enable tailscale systray (user service)
 install -d /usr/lib/systemd/user /etc/systemd/user/default.target.wants
--- system_files/etc/profile.d/blesh.sh
+++ system_files/etc/profile.d/blesh.sh
@@ -1,9 +1,15 @@
 [[ -n "${BASH_VERSION:-}" ]] || return 0
 
-[[ $- == *i* ]] && source -- /usr/share/blesh/ble.sh --attach=none
-if [[ ${BLE_VERSION-} ]]; then
+if [[ ! ${__BLESH_SYSTEM_RC_LOADED-} ]]; then
+  __BLESH_SYSTEM_RC_LOADED=1
+
+  _ble_rcfile=/etc/blerc
+
+  if [[ -r /usr/share/blesh/ble.sh ]]; then
+    source -- /usr/share/blesh/ble.sh --attach=none --rcfile "$_ble_rcfile"
+  elif [[ -r /usr/share/blesh/ble.sh.bash ]]; then
+    source -- /usr/share/blesh/ble.sh.bash --attach=none --rcfile "$_ble_rcfile"
+  fi
+
   _ble_contrib_fzf_base=/usr/share/fzf
-  ble-import -d integration/fzf-completion
-  ble-import -d integration/fzf-key-bindings
 fi
-[[ ! ${BLE_VERSION-} ]] || ble-attach
