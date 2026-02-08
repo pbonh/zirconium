c0cdd0b7b6e072fef64013afc228a934f866c586
Tulip Blossom <tulilirockz@outlook.com>
2026-02-08 10:57:00 -0300

    chore: dont touch `/usr/src` ig
    Jackrabbit builds failed because the nvidia kmod couldn't be found
    
    Signed-off-by: Tulip Blossom <tulilirockz@outlook.com>

--- build_files/99-misc-post.sh
+++ build_files/99-misc-post.sh
@@ -29,7 +29,6 @@ rm -rf /usr/lib/systemd/system/flatpak-add-fedora-repos.service
 systemctl enable flatpak-add-flathub-repos.service
 
 rm -rf /usr/bin/chsh # footgun
-rm -rf /usr/src # we dont need anything here
 
 systemctl enable rechunker-group-fix.service
 
